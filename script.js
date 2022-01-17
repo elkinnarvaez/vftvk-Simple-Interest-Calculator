9e2c998a1075433cc21eb2102bc96916f62f985c
Elkin Narvaez <elkinnarvaez2406@gmail.com>
2022-01-17 17:52:12 -0500

    Webpage finished

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    // Getting DOM elements values
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+
+    // Validation of the principal entry
+    if(isNaN(parseInt(principal)) || parseInt(principal) <= 0){
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+    }
+    else{
+        // Computation of the interest
+        var amount = principal * years * rate /100;
+        // Computation of the year
+        var year = new Date().getFullYear()+parseInt(years);
     
+        document.getElementById("result").innerHTML="If you deposit "+"<mark>"+principal+"</mark>"+",\<br\>at an interest rate of "+"<mark>"+rate+"%</mark>\<br\>You will receive an amount of "+"<mark>"+amount+"</mark>"+",\<br\>in the year "+"<mark>"+year+"</mark>"+"\<br\>";
+    }
 }
-        
+
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval + "%";
+}
